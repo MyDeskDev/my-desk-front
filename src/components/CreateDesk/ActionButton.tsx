9d335eaaf9451902e1483365c13bd5077ca4aefe
Jin Kwan Kim <47937211+kkole3897@users.noreply.github.com>
2022-07-14 03:11:07 +0900

    CreateDesk ActionButton 추가

---
+++ src/components/CreateDesk/ActionButton.tsx
@@ -0,0 +1,35 @@
+import { Button } from '@chakra-ui/react';
+import type { ButtonProps } from '@chakra-ui/react';
+
+type Props = Pick<ButtonProps, 'h' | 'bgColor' | 'fontSize' | 'children'>;
+
+const ActionButton = (props: Props) => {
+  const { h = '50px', bgColor = 'black', fontSize = '1.8rem' } = props;
+
+  return (
+    <Button
+      h={h}
+      bgColor={bgColor}
+      border="3px solid #000000"
+      borderRadius="0"
+      color="#FFFFFF"
+      fontSize={fontSize}
+      fontWeight="700"
+      lineHeight="2.4rem"
+      flex="1"
+      _hover={{
+        bgColor,
+      }}
+      _focus={{
+        bgColor,
+      }}
+      _active={{
+        bgColor,
+      }}
+    >
+      {props.children}
+    </Button>
+  );
+};
+
+export default ActionButton;

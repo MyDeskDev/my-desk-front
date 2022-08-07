b61b2e3d93332c9a5f22ad22c22f27ba3284c617
Jin Kwan Kim <47937211+kkole3897@users.noreply.github.com>
2022-08-07 12:53:22 +0900

    Textarea forwardRef 추가

--- src/components/CreateDesk/Textarea.tsx
+++ src/components/CreateDesk/Textarea.tsx
@@ -1,26 +1,36 @@
 import { Textarea } from '@chakra-ui/react';
+import React, { ChangeEventHandler } from 'react';
 
 interface Props {
   placeholder?: string;
   value?: string;
+  name?: string;
+  onChange?: ChangeEventHandler<HTMLTextAreaElement>;
 }
 
-const CustomTextarea = (props: Props) => {
-  return (
-    // TODO: auto resizing 적용
-    <Textarea
-      placeholder={props.placeholder}
-      value={props.value}
-      p="10px 20px"
-      border="0"
-      borderRadius="0"
-      bgColor="#F3F3F3"
-      resize="none"
-      fontSize="1.6rem"
-      lineHeight="2.4rem"
-      _placeholder={{ color: '#C4C4C4' }}
-    />
-  );
-};
+const CustomTextarea = React.forwardRef<HTMLTextAreaElement, Props>(
+  (props, ref) => {
+    return (
+      // TODO: auto resizing 적용
+      <Textarea
+        placeholder={props.placeholder}
+        value={props.value}
+        name={props.name}
+        p="10px 20px"
+        border="0"
+        borderRadius="0"
+        bgColor="#F3F3F3"
+        resize="none"
+        fontSize="1.6rem"
+        lineHeight="2.4rem"
+        _placeholder={{ color: '#C4C4C4' }}
+        onChange={props.onChange}
+        ref={ref}
+      />
+    );
+  }
+);
+
+CustomTextarea.displayName = 'CustomTextarea';
 
 export default CustomTextarea;

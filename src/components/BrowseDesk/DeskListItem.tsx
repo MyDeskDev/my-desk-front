063d53a4d15d53ae0af7984e4a198923b932e85a
Jin Kwan Kim <47937211+kkole3897@users.noreply.github.com>
2022-09-29 00:41:29 +0900

    DeskListItem 반응형 maxW 추가

--- src/components/BrowseDesk/DeskListItem.tsx
+++ src/components/BrowseDesk/DeskListItem.tsx
@@ -8,7 +8,12 @@ const DeskListItem = (props: Props) => {
   const { children } = props;
 
   return (
-    <Box maxW="33.3%" w="100%" p="0 39px">
+    <Box
+      w="100%"
+      maxW={{ base: '100%', md: '33.3%', lg: '33.3%', xl: '33.3%' }}
+      minW="1px"
+      p="0 39px"
+    >
       {children}
     </Box>
   );

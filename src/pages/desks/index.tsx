7d515d110e08a85ae7d51573636cd0703b50b207
Jin Kwan Kim <47937211+kkole3897@users.noreply.github.com>
2022-09-29 01:08:20 +0900

    desks 페이지 ui 추가

--- src/pages/desks/index.tsx
+++ src/pages/desks/index.tsx
@@ -1,12 +1,49 @@
+import Link from 'next/link';
 import type { NextPage } from 'next';
 
 import DeskCard from '@/components/BrowseDesk/DeskCard';
+import DeskList from '@/components/BrowseDesk/DeskList';
+import DeskListItem from '@/components/BrowseDesk/DeskListItem';
+import BaseContainer from '@/components/layouts/Base/BaseContainer';
+import BaseHeader from '@/components/layouts/Base/BaseHeader';
 
 const Desks: NextPage = () => {
   return (
     <div>
+      <BaseHeader />
       <main>
-        <DeskCard deskId={1} avatar={<></>} />
+        <BaseContainer>
+          <DeskList>
+            <DeskListItem>
+              <Link href="#">
+                <a>
+                  <DeskCard deskId={1} avatar={<></>} />
+                </a>
+              </Link>
+            </DeskListItem>
+            <DeskListItem>
+              <Link href="#">
+                <a>
+                  <DeskCard deskId={1} avatar={<></>} />
+                </a>
+              </Link>
+            </DeskListItem>
+            <DeskListItem>
+              <Link href="#">
+                <a>
+                  <DeskCard deskId={1} avatar={<></>} />
+                </a>
+              </Link>
+            </DeskListItem>
+            <DeskListItem>
+              <Link href="#">
+                <a>
+                  <DeskCard deskId={1} avatar={<></>} />
+                </a>
+              </Link>
+            </DeskListItem>
+          </DeskList>
+        </BaseContainer>
       </main>
     </div>
   );

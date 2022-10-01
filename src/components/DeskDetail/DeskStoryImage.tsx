d34d4042785c7f768b1394256a045a3853fd325a
Jin Kwan Kim <47937211+kkole3897@users.noreply.github.com>
2022-10-02 04:00:52 +0900

    DeskStory 관련 ui 추가

---
+++ src/components/DeskDetail/DeskStoryImage.tsx
@@ -0,0 +1,11 @@
+import { Box, Image } from '@chakra-ui/react';
+
+const DeskStoryImage = () => {
+  return (
+    <Box maxW="1200px" margin="0 auto" padding="5px 0">
+      <Image src="" alt="" w="100%" />
+    </Box>
+  );
+};
+
+export default DeskStoryImage;
--- src/pages/desks/[id].tsx
+++ src/pages/desks/[id].tsx
@@ -8,6 +8,8 @@ import UserProfileImage from '@/components/DeskDetail/UserProfileImage';
 import UserSummary from '@/components/DeskDetail/UserSummary';
 import DeskTypeContainer from '@/components/DeskDetail/DeskTypeContainer';
 import DeskSummary from '@/components/DeskDetail/DeskSummary';
+import DeskStoryText from '@/components/DeskDetail/DeskStoryText';
+import DeskStoryImage from '@/components/DeskDetail/DeskStoryImage';
 
 const Desk: NextPage = () => {
   return (
@@ -21,6 +23,29 @@ const Desk: NextPage = () => {
         </Box>
         <DeskTypeContainer />
         <DeskSummary />
+        <Box>
+          <DeskStoryText>
+            안녕하세요. 저는 태그바이에서 PO로 일하고 있는 김경수 라고 합니다.
+          </DeskStoryText>
+          <DeskStoryImage />
+          <DeskStoryText>
+            안방에 있던 콘솔을 거실로 옮겨 귀여운 책상공간을 만들었어요. 나름
+            맘에들어 당분간 이대로 두기로 그리고 몇일전 봉자한테 귀여운 인터뷰
+            요청이 들어왔어요. 뉴스레터로 소개되는건데 맘껏 자랑할 수 있는
+            기회라 바로 하기로 질문답변 하나씩 끄적끄적 적어가고있어요 안방에
+            있던 콘솔을 거실로 옮겨 귀여운 책상공간을 만들었어요. 나름 맘에들어
+            당분간 이대로 두기로 그리고 몇일전 봉자한테 귀여운 인터뷰 요청이
+            들어왔어요. 뉴스레터로 소개되는건데 맘껏 자랑할 수 있는 기회라 바로
+            하기로 질문답변 하나씩 끄적끄적 적어가고있어요 안방에 있던 콘솔을
+            거실로 옮겨 귀여운 책상공간을 만들었어요. 나름 맘에들어 당분간
+            이대로 두기로 그리고 몇일전 봉자한테 귀여운 인터뷰 요청이
+            들어왔어요. 뉴스레터로 소개되는건데 맘껏 자랑할 수 있는 기회라 바로
+            하기로 질문답변 하나씩 끄적끄적 적어가고있어요
+          </DeskStoryText>
+          <DeskStoryImage />
+          <DeskStoryImage />
+          <DeskStoryImage />
+        </Box>
       </main>
     </div>
   );

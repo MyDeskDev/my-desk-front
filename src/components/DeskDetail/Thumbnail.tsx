18d20f672325867e7a67f6eb589c4c43e9133650
Jin Kwan Kim <47937211+kkole3897@users.noreply.github.com>
2022-10-01 23:15:03 +0900

    DeskDetail Thumbnail ui 추가

---
+++ src/components/DeskDetail/Thumbnail.tsx
@@ -0,0 +1,11 @@
+import { Image, Box } from '@chakra-ui/react';
+
+const DeskThumbnail = () => {
+  return (
+    <Box h={{ base: '270px', lg: '600px' }}>
+      <Image src="" alt="" w="100%" h="100%" objectFit="cover" />
+    </Box>
+  );
+};
+
+export default DeskThumbnail;
--- src/pages/desks/[id].tsx
+++ src/pages/desks/[id].tsx
@@ -1,9 +1,16 @@
 import type { NextPage } from 'next';
 
+import BaseHeader from '@/components/layouts/Base/BaseHeader';
+import BaseContainer from '@/components/layouts/Base/BaseContainer';
+import DeskThumbnail from '@/components/DeskDetail/Thumbnail';
+
 const Desk: NextPage = () => {
   return (
     <div>
-      <main>데스크 상세</main>
+      <BaseHeader />
+      <main>
+        <DeskThumbnail />
+      </main>
     </div>
   );
 };

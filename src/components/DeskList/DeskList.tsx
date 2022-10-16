ace3180a451385a8fed3f00691e5d33f7bfc0544
Jin Kwan Kim <47937211+kkole3897@users.noreply.github.com>
2022-10-16 15:44:28 +0900

    DeskListSkeleton 추가

--- src/components/DeskList/DeskList.tsx
+++ src/components/DeskList/DeskList.tsx
@@ -1,16 +1,32 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Skeleton } from '@chakra-ui/react';
 import Link from 'next/link';
 
 import DeskListItem from '@/components/DeskList/DeskListItem';
 import DeskCard from '@/components/DeskList/DeskCard';
+import DeskCardSkeleton from '@/components/DeskList/DeskCardSkeleton';
 
 import useDesksQuery from '@/hooks/useDesksQuery';
+import useDeskListSkeleton from '@/hooks/useDeskListSkeleton';
 
 const DeskList = () => {
   const { data: desks, isLoading } = useDesksQuery();
 
+  const { skeletonLength } = useDeskListSkeleton({ defaultLength: 30 });
+
   if (isLoading) {
-    return <div>로딩중</div>;
+    return (
+      <Flex flexWrap="wrap" margin="0 -39px">
+        {Array(skeletonLength)
+          .fill(0)
+          .map((_, index) => {
+            return (
+              <DeskListItem key={index}>
+                <DeskCardSkeleton />
+              </DeskListItem>
+            );
+          })}
+      </Flex>
+    );
   } else if (desks?.length === 0) {
     return <div>아직 등록된 책상이 없습니다.</div>;
   }

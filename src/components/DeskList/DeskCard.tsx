7478a1dce7adfea1073e04f72e29a336d9eadc19
Jin Kwan Kim <47937211+kkole3897@users.noreply.github.com>
2022-11-05 16:30:08 +0900

    DeskCard 이미지 floating 테두리 추가

--- src/components/DeskList/DeskCard.tsx
+++ src/components/DeskList/DeskCard.tsx
@@ -13,43 +13,67 @@ const DeskCard = (props: Props) => {
     <Box>
       <Box
         position="relative"
-        overflow="hidden"
         pt="251px"
         borderRadius="20px 20px 0 0"
-        bgColor="lightgray"
+        _after={{
+          content: '""',
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+          border: '1px solid rgba(0, 0, 0, 0.07)',
+          borderRadius: '20px 20px 0 0',
+        }}
       >
-        <Image
-          src={desk.mainImgUrl}
-          alt=""
-          loading="lazy"
-          position="absolute"
-          top="50%"
-          right="50%"
-          bottom="50%"
-          left="50%"
-          w="100%"
-          h="100%"
-          objectFit="cover"
-          transform="translate(-50%, -50%)"
-        />
+        {desk.thumbnailImgUrl && (
+          <Image
+            src={desk.thumbnailImgUrl}
+            alt=""
+            loading="lazy"
+            position="absolute"
+            top="50%"
+            right="50%"
+            bottom="50%"
+            left="50%"
+            w="100%"
+            h="100%"
+            borderRadius="20px 20px 0 0"
+            objectFit="cover"
+            transform="translate(-50%, -50%)"
+          />
+        )}
       </Box>
       <Flex h="80px" mt="-36px" justifyContent="center" zIndex={1}>
         <Box
+          position="relative"
           borderRadius="50%"
           w="80px"
           h="80px"
           zIndex={1}
-          bgColor="gray"
           overflow="hidden"
+          bgColor="#fff"
+          _after={{
+            content: '""',
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            border: '1px solid rgba(0, 0, 0, 0.07)',
+            borderRadius: '50%',
+          }}
         >
-          <Image
-            src={desk.user.profileImgUrl}
-            alt=""
-            loading="lazy"
-            w="100%"
-            h="100%"
-            objectFit="cover"
-          />
+          {desk.user.profileImgUrl && (
+            <Image
+              src={desk.user.profileImgUrl}
+              alt=""
+              loading="lazy"
+              w="100%"
+              h="100%"
+              objectFit="cover"
+            />
+          )}
         </Box>
       </Flex>
       <Box p="15px 49px 49px">

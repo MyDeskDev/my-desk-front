4d3f946f31778e19562ec5132b20737b9e380d97
Jin Kwan Kim <47937211+kkole3897@users.noreply.github.com>
2023-02-04 15:48:13 +0900

    기본 폰트 설정 변경

--- src/styles/fonts.tsx
+++ src/styles/fonts.tsx
@@ -1,13 +1,43 @@
 import { Global, css } from '@emotion/react';
 
+const styles = css`
+  @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.4/dist/web/static/pretendard.css');
+  @font-face {
+    font-family: 'Apple SD Gothic Neo';
+    src: url('/fonts/AppleSDGothicNeoR.woff2') format('woff2'),
+      url('/fonts/AppleSDGothicNeoR.woff') format('woff');
+    font-weight: 400;
+  }
+  @font-face {
+    font-family: 'Apple SD Gothic Neo';
+    src: url('/fonts/AppleSDGothicNeoM.woff2') format('woff2'),
+      url('/fonts/AppleSDGothicNeoM.woff') format('woff');
+    font-weight: 500;
+  }
+  @font-face {
+    font-family: 'Apple SD Gothic Neo';
+    src: url('/fonts/AppleSDGothicNeoSB.woff2') format('woff2'),
+      url('/fonts/AppleSDGothicNeoSB.woff') format('woff');
+    font-weight: 600;
+  }
+  @font-face {
+    font-family: 'Apple SD Gothic Neo';
+    src: url('/fonts/AppleSDGothicNeoB.woff2') format('woff2'),
+      url('/fonts/AppleSDGothicNeoB.woff') format('woff');
+    font-weight: 700;
+  }
+  @font-face {
+    font-family: 'MapoFlowerIsland';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/MapoFlowerIslandA.woff')
+      format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+  @import url('https://fonts.cdnfonts.com/css/antihero');
+`;
+
 const Fonts = () => {
-  return (
-    <Global
-      styles={css`
-        @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.4/dist/web/static/pretendard.css');
-      `}
-    />
-  );
+  return <Global styles={styles} />;
 };
 
 export default Fonts;
---
+++ src/styles/variables.ts
@@ -0,0 +1,4 @@
+export const appleSdGothicFont =
+  "'Apple SD Gothic Neo', -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif";
+export const mapoFlowerIslandFont = "'MapoFlowerIsland', serif;";
+export const antiheroFont = "'Antihero', sans-serif;";

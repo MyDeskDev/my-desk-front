a4a48308943234ec73a9da269ff1cbe820c0ee45
dev-yhc <hyunchul.yang@gmail.com>
2023-02-26 14:30:46 +0900

    fix: container update script 수정

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,15 +1,17 @@
-echo "> 새로운 이미지 빌드"
 date=$(date '+%Y%m%d%H%M%S')
-docker build -t mydesk-front:$date .
+IMAGE_NAME=mydesk-front:$date
+
+echo "> 새로운 이미지 빌드"
+docker build -t $IMAGE_NAME .
 
 echo "> 기존 container 찾기"
-CONTAINER_ID=$(docker ps | awk '{ if ($2 == "mydesk-front") print ($1)}')
+CONTAINER_ID=$(docker ps | grep mydesk-front | awk '{ print $1 }')
 
 echo "> stop & remove"
 docker stop $CONTAINER_ID && docker rm $CONTAINER_ID
 
 echo "> 새로운 container 띄우기"
-NEW_CONTAINER_ID=$(docker container run -d -p 3000:3000 mydesk-front)
+NEW_CONTAINER_ID=$(docker container run -d -p 3000:3000 --name=mydesk-front $IMAGE_NAME)
 
 echo "> 성공적으로 띄워졌는지 확인"
 sleep 5
